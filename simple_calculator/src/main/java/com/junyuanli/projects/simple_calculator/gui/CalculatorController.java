9d3305ba877c6e7dbe4042351049264d91b79290
Junyuan Li <junyuan.li@outlook.com>
2023-03-03 22:29:41 -0500

    added basic number sign clear buttons to gui

--- simple_calculator/src/main/java/com/junyuanli/projects/simple_calculator/gui/CalculatorController.java
+++ simple_calculator/src/main/java/com/junyuanli/projects/simple_calculator/gui/CalculatorController.java
@@ -1,6 +1,8 @@
 package com.junyuanli.projects.simple_calculator.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class CalculatorController {
@@ -8,7 +10,25 @@ public class CalculatorController {
     private TextField calculatorDisplay;
 
     @FXML
-    protected void onNumberTwoClick() {
-        calculatorDisplay.appendText("2");
+    protected void onNumberButtonClick(ActionEvent event) {
+        Button button = (Button) event.getSource();
+        calculatorDisplay.appendText(button.getText());
     }
+
+    @FXML
+    protected void onSwitchSignButtonClick() {
+        String calculatorDisplayText = calculatorDisplay.getText();
+        if (calculatorDisplayText.startsWith("-")) {
+            calculatorDisplayText = calculatorDisplayText.substring(1);
+        } else {
+            calculatorDisplayText = "-".concat(calculatorDisplayText);
+        }
+        calculatorDisplay.setText(calculatorDisplayText);
+    }
+
+    @FXML
+    protected void onClearButtonClick() {
+        calculatorDisplay.clear();
+    }
+
 }
