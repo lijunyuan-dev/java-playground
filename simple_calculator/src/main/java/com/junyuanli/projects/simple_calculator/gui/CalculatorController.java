483586113ab5503722298e05f6cc8937ce89208e
Junyuan Li <junyuan.li@outlook.com>
2023-03-05 22:47:10 -0500

    added initialization code

--- simple_calculator/src/main/java/com/junyuanli/projects/simple_calculator/gui/CalculatorController.java
+++ simple_calculator/src/main/java/com/junyuanli/projects/simple_calculator/gui/CalculatorController.java
@@ -1,24 +1,44 @@
 package com.junyuanli.projects.simple_calculator.gui;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class CalculatorController {
+    @FXML
+    private GridPane calculatorWindow;
     @FXML
     private TextField calculatorDisplay;
 
     @FXML
-    protected void onNumberButtonClick(ActionEvent event) {
-        Button button = (Button) event.getSource();
-        calculatorDisplay.appendText(button.getText());
+    public void initialize() {
+        calculatorDisplay.setText("0");
+        Platform.runLater(() -> calculatorWindow.requestFocus());
     }
 
     @FXML
-    protected void onSwitchSignButtonClick() {
+    private void onNumberButtonClick(ActionEvent event) {
+        if (event.getSource() instanceof Button button) {
+            if (calculatorDisplay.getText().equals("0")) {
+                calculatorDisplay.replaceText(0, 1, button.getText());
+                return;
+            }
+            calculatorDisplay.appendText(button.getText());
+            return;
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    @FXML
+    private void onSwitchSignButtonClick() {
         String processedDisplayText;
         String calculatorDisplayText = calculatorDisplay.getText();
+        if (calculatorDisplayText.equals("0")) {
+            return;
+        }
         if (calculatorDisplayText.startsWith("-")) {
             processedDisplayText = calculatorDisplayText.substring(1);
         } else {
@@ -28,13 +48,13 @@ public class CalculatorController {
     }
 
     @FXML
-    protected void onClearButtonClick() {
+    private void onClearButtonClick() {
         calculatorDisplay.clear();
+        calculatorDisplay.setText("0");
     }
 
     @FXML
-    protected void onOperatorButtonClick() {
+    private void onOperatorButtonClick() {
 
     }
-
 }
