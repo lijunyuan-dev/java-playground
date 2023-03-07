cb0d33364e2177b7055b34ad72c45c5805f8c989
Junyuan Li <junyuan.li@outlook.com>
2023-03-07 16:11:29 -0500

    updated calculator core logic

--- simple_calculator/src/main/java/com/junyuanli/projects/simple_calculator/core/CalculatorStateManager.java
+++ simple_calculator/src/main/java/com/junyuanli/projects/simple_calculator/core/CalculatorStateManager.java
@@ -1,13 +1,30 @@
 package com.junyuanli.projects.simple_calculator.core;
 
-/**
- * Manages the calculator's state.
- * */
+import java.util.HashMap;
 
-public class CalculatorStateManager {
-    private static Operation currentOperation = null;
+public final class CalculatorStateManager {
 
-    public static void updateOperation(String operation) {
+    // Singleton Pattern
+    private static final CalculatorStateManager manager = new CalculatorStateManager();
+    private final HashMap<String, Operation> allOperations = new HashMap<>();
+    private Operation currentOperation = null;
+    private int numberOfOperands = 0;
 
+    // Prevent out-of-class instantiation
+    private CalculatorStateManager() {}
+
+    public static CalculatorStateManager initialize() {
+        initializeOperations();
+        return manager;
+    }
+
+    private static void initializeOperations() {
+        manager.allOperations.putIfAbsent("+", new Addition());
+        manager.allOperations.putIfAbsent("-", new Subtraction());
+        manager.allOperations.putIfAbsent("×", new Multiplication());
+    }
+
+    public void updateOperation(String operation) {
+        currentOperation = allOperations.get(operation);
     }
 }
--- simple_calculator/src/main/java/com/junyuanli/projects/simple_calculator/core/Operation.java
+++ simple_calculator/src/main/java/com/junyuanli/projects/simple_calculator/core/Operation.java
@@ -1,23 +1,45 @@
 package com.junyuanli.projects.simple_calculator.core;
 
 public abstract class Operation {
-    public abstract double calculate();
+    public abstract double calculate() throws Division.ZeroDivisionException;
 }
 
 abstract class BinaryOperation extends Operation {
+    protected double operand1;
+    protected double operand2;
+
 
 }
 
-class Addition extends Operation {
+class Addition extends BinaryOperation {
     @Override
     public double calculate() {
-        return 0;
+        return operand1 + operand2;
     }
 }
 
-class Subtraction extends Operation {
+class Subtraction extends BinaryOperation {
     @Override
     public double calculate() {
-        return 0;
+        return operand1 - operand2;
+    }
+}
+
+class Multiplication extends BinaryOperation {
+    @Override
+    public double calculate() {
+        return operand1 * operand2;
+    }
+}
+
+class Division extends BinaryOperation {
+    @Override
+    public double calculate() throws ZeroDivisionException {
+        if (operand2 == 0.0d) {
+            throw new ZeroDivisionException();
+        }
+        return operand1 * operand2;
+    }
+    static class ZeroDivisionException extends Exception {
     }
 }
